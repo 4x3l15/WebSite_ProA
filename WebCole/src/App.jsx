0b6adcd3719b7913409fe9f550c968e3ab5d34a8
DDHGDBNHDUEU <mbrodriguez@escuelasproa.edu.ar>
2025-09-01 11:53:19 -0300

    comentario de dependencias

--- WebCole/src/App.jsx
+++ WebCole/src/App.jsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 /* INSTALAR BOOTSTRAP ANTES DE CORRER EL CODIGO POR QUE SI NO ANDA, npm i bootstrap */
+/* INSTALAR EN CONSOLA npm init -y npm install express mongoose cors*/
 import Navbar from "./componentes/Navbar";
 import Footer from "./componentes/Footer";
 import Jumbotron from "./componentes/Jumbotron";
